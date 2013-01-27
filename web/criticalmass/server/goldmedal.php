ed1c07ebffe3eeb2ed04cb8f31aa388bc7ceb152
XxzatoichixX <gaston.torres96@hotmail.com>
2013-01-27 21:50:06 -0200

    Update web/criticalmass/server/goldmedal.php

--- web/criticalmass/server/goldmedal.php
+++ web/criticalmass/server/goldmedal.php
@@ -1,4 +1,26 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+ <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+
+ <head>
+   <meta property="og:title" content="3 Ball Combo" />
+   <meta property="og:type" content="game.achievement" />
+   <meta property="game:points" content="50" />
+   <meta property="og:image" content="http://DOMAIN_NAME/criticalmasscomplete/server/goldmedal.png" />
+   <meta property="og:description" content="Gold Medal - destroy 3 balls with a single explosion!" />
+   <meta property="og:url" content="http://DOMAIN_NAME/criticalmasscomplete/server/goldmedal.php" />
+   <meta property="fb:app_id" content="APP_ID" />
+ </head>
+ <body>
+
+   <div style="text-align:center; width:500px; margin:0px auto;">
+     <img src='http://DOMAIN_NAME/criticalmasscomplete/server/goldmedal.png' />
+     <div>Gold Medal - destroy 3 balls with a single explosion!</div>
+     <div><a href="http://apps.facebook.com/APP_NAMESPACE/">Play Critical Mass Now!</a><div>
+   </div>  
+
+ </body>
+ </html>
+ <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
 
 <head>
